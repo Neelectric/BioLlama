e91d588c57b97e45c8f7e1275902a73f4677511f
Neelectric <Neel.R@web.de>
2023-09-28 23:30:06 +0100

    Adding parameters to db_build.py and yml support

--- db_build.py
+++ db_build.py
@@ -1,21 +1,29 @@
 import box
 import yaml
 import glob
+import argparse
 import faiss
 from tqdm import tqdm
+from sentence_transformers import SentenceTransformer
 
 with open('config/config.yml', 'r', encoding='utf8') as ymlfile:
     cfg = box.Box(yaml.safe_load(ymlfile))
 
-def build_index():
+def build_index(db_name):
     chunk = ""
     chunks = []
     n_lines = 0
-    txt_files = glob.glob(cfg.DATA_PATH + "/*.txt")
-    if len(txt_files) != 1:
-        raise Exception("There should be exactly one .txt file in the data directory.")
+
+    txt_files = glob.glob(cfg.DATABASE_PATH + "/*.txt")
+    print(txt_files)
+    if db_name in txt_files:
+        file_name = db_name
+    else:
+        file_name = txt_files[0]
+    # if len(txt_files) != 1:
+    #     raise Exception("There should be exactly one .txt file in the data directory.")
     
-    with open(txt_files[0], 'r') as file:
+    with open(file_name, 'r') as file:
         for line in tqdm(file, desc="Building index",):
             n_lines += 1
             #print(n_lines)
@@ -24,7 +32,15 @@ def build_index():
             else:
                 chunk += line
     print(len(chunks))
-    
+    embedding_model = SentenceTransformer("thenlper/gte-large")
+    chunk_embeddings = embedding_model.encode(chunks)
+
+    index = faiss.IndexFlatL2(chunk_embeddings.shape[1])
+    index.add(chunk_embeddings)
+    faiss.write_index(index, cfg.DATABASE_AS_FAISS_PATH + db_name + '.index')
 
 if __name__ == "__main__":
-    build_index()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('db_name', type=str, help="Name of the database to build index for.")
+    args = parser.parse_args()
+    build_index(args.db_name)
