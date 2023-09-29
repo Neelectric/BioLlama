0b1af68548e5577181e12a8d8300c85d46317b15
Neelectric <Neel.R@web.de>
2023-09-29 00:13:45 +0000

    Minor FAISS changes

--- db_build.py
+++ db_build.py
@@ -2,6 +2,8 @@ import box
 import yaml
 import glob
 import argparse
+import time
+import json
 import faiss
 from tqdm import tqdm
 from sentence_transformers import SentenceTransformer
@@ -12,35 +14,51 @@ with open('config/config.yml', 'r', encoding='utf8') as ymlfile:
 def build_index(db_name):
     chunk = ""
     chunks = []
+    n_chunks = 0
     n_lines = 0
+    chunks_dict = {}
 
     txt_files = glob.glob(cfg.DATABASE_PATH + "/*.txt")
     print(txt_files)
     if db_name in txt_files:
         file_name = db_name
+        print("Found the requested file, building FAISS index")
     else:
         file_name = txt_files[0]
     # if len(txt_files) != 1:
     #     raise Exception("There should be exactly one .txt file in the data directory.")
     
     with open(file_name, 'r') as file:
-        for line in tqdm(file, desc="Building index",):
+        for line in tqdm(file, desc="Processing chunks",):
             n_lines += 1
             #print(n_lines)
             if not line.strip():
+                n_chunks += 1
                 chunks.append(chunk)
+                chunks_dict[n_chunks] = chunk
+                chunk = ""
             else:
                 chunk += line
+            
     print(len(chunks))
+    time_before_index = time.time()
     embedding_model = SentenceTransformer("thenlper/gte-large")
     chunk_embeddings = embedding_model.encode(chunks)
-
+    print("Embeddings shape: " + str(chunk_embeddings.shape))
     index = faiss.IndexFlatL2(chunk_embeddings.shape[1])
     index.add(chunk_embeddings)
+    print(cfg.DATABASE_AS_FAISS_PATH + db_name + '.index')
     faiss.write_index(index, cfg.DATABASE_AS_FAISS_PATH + db_name + '.index')
+    time_after_index = time.time()
+    print("Time to build index: " + str(time_after_index - time_before_index) + " seconds.")
+
+    #save to JSON
+    json_file_path = cfg.DATABASE_AS_JSON_PATH + db_name + '.json'
+    with open(json_file_path, "w") as json_file:
+        json.dump(chunks_dict, json_file, indent=4)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('db_name', type=str, help="Name of the database to build index for.")
+    parser.add_argument('--db_name', type=str, default="train.txt", help="Name of the database to build index for.")
     args = parser.parse_args()
     build_index(args.db_name)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import box
 import yaml
 import faiss
-
+import json
+import numpy as np
 from src.utilities import load_benchmark, load_knowledge_db
 
 from sentence_transformers import SentenceTransformer
@@ -11,14 +12,37 @@ with open('config/config.yml', 'r', encoding='utf8') as ymlfile:
 
 
 model = "Llama-2-70B-q4"
-benchmark = load_benchmark("BioASQ-training5b")
-knowledge_db = load_knowledge_db()
+benchmark = "BioASQ-training5b/BioASQ-trainingDataset5b.json"
+question_type = "factoid"
+database_name = "train.txt"
+k = 5
+
+questions, exact_answers = load_benchmark(benchmark, question_type)
+print("Loaded benchmark" + benchmark + " with " + str(len(questions)) + " questions and " + str(len(exact_answers)) + " answers.")
+knowledge_db = load_knowledge_db(database_name)
 embedding_model = SentenceTransformer("thenlper/gte-large")
 responses = []
 
-for question in benchmark:
-    question_embedding = embedding_model.encode(question)
-    similar_chunks = knowledge_db.get_similar_chunks(question_embedding)
-    prompt = question + " " + similar_chunks
-    response = model(prompt)
-    responses.append(response)
+JSON_file_path = cfg.DATABASE_AS_JSON_PATH + database_name + '.json'
+print(JSON_file_path)
+with open(JSON_file_path, "r") as json_file:
+    # Load the JSON data into a Python dictionary
+    knowledge_db_as_JSON = json.load(json_file)
+
+for question in questions[0:4]:
+    print(question)
+    question_embedding = np.array([embedding_model.encode(question)])
+    #print("Question embedding shape: " + str(question_embedding.shape))
+    #D, I = index.search(xb[:5], k)
+    distances, indices = knowledge_db.search(question_embedding, k)
+    distances = distances.flatten()
+    indices = indices.flatten()
+    print("Distances shape: " + str(distances.shape))
+
+    for i in range(len(distances)):
+        print("Distance: " + str(distances[i]) + " Index: " + str(indices[i]))
+        print(knowledge_db_as_JSON[int(indices[i])])
+    prompt = question + " " + "something"
+    print("\n")
+    # response = model(prompt)
+    # responses.append(response)
