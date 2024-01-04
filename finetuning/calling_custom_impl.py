cd56072cd65ab8f8c9196a4cb9c414fd1061bf95
Neelectric <Neel.R@web.de>
2024-01-04 22:00:19 +0000

    Adding transformers, optimum and AutoGPTQ to repo...

--- finetuning/calling_custom_impl.py
+++ finetuning/calling_custom_impl.py
@@ -1,6 +1,6 @@
-from finetuning.cti.transformers.transformers.src.transformers.models.auto.modeling_auto import AutoModelForCausalLM as AutoModelForCausalLM
-
-from finetuning.cti.transformers.transformers.src.transformers.models.auto.tokenization_auto import AutoTokenizer as AutoTokenizer
+from cti.transformers.transformers.src.transformers.models.auto.modeling_auto import AutoModelForCausalLM as AutoModelForCausalLM
+from cti.transformers.transformers.src.transformers.models.auto.tokenization_auto import AutoTokenizer as AutoTokenizer
+import time
 
 model_name_or_path = "TheBloke/Llama-2-7b-Chat-GPTQ"
 # To use a different branch, change revision
@@ -16,6 +16,10 @@ print("\n\n*** Generate:")
 
 prompt_template = "Tell me about AI please"
 
+time_before_generation = time.time()
 input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
-output = model.generate(inputs=input_ids, temperature=0.7, do_sample=True, top_p=0.95, top_k=40, max_new_tokens=512)
+output = model.generate(inputs=input_ids, temperature=0.01, do_sample=True, top_p=0.95, top_k=40, max_new_tokens=30)
 print(tokenizer.decode(output[0]))
+time_after_generation = time.time()
+
+print(f"time for generaiton: {time_after_generation - time_before_generation}")
