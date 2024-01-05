ed3993d03dd046a7a8a85fb825f8a53a161d265b
Neelectric <Neel.R@web.de>
2024-01-05 13:56:01 +0000

    Small reshuffle to add support for local transformers library usage

--- utilities/biollama.py
+++ utilities/biollama.py
@@ -4,11 +4,16 @@
 # Hugely aided by https://github.com/nrimsky/LM-exp/blob/main/intermediate_decoding/intermediate_decoding.ipynb
 
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+from ..finetuning.cti.transformers.transformers.src.transformers.models.auto import AutoTokenizer, AutoModelForCausalLM
 import time
 from .db_retrieval import medcpt_FAISS_retrieval
 
-# class RETROWrapper(torch.nn.Module):
+class RETROWrapper(torch.nn.Module):
+    def __init__(self, id, layer):
+        super().__init__()
+        print(f"Wrapping layer {id} with retro")
+        self.layer = layer
 
 class BioLlama:
     def __init__(self, model_id, chunk_length):
@@ -22,11 +27,11 @@ class BioLlama:
         # for i, layer in enumerate(self.model.model.layers):
         #     self.model.model.layers[i] = BlockOutputWrapper(layer, self.model.lm_head, self.model.model.norm)
 
-        # RETRO_layer_ids = [15]
-        # for i, layer in enumerate(self.model.model.layers):
-        #     #switch decoder layer to be a RETRO layer
-        #     if i in RETRO_layer_ids:
-        #         self.model.model.layers[i] = RETROWrapper(layer, self.model.lm_head, self.model.model.norm)
+        RETRO_layer_ids = [15]
+        for i, layer in enumerate(self.model.model.layers):
+            #switch decoder layer to be a RETRO layer
+            if i in RETRO_layer_ids:
+                self.model.model.layers[i] = RETROWrapper(id=i, layer=layer)
 
     def inference(self, questions, db_name, retrieval_text_mode):
         #generate neighbours
