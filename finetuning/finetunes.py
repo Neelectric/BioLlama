ed0a03c2525a090aa67027b85912316b59a62c61
Neelectric <Neel.R@web.de>
2024-01-05 08:46:53 +0000

    Minor change

--- finetuning/finetunes.py
+++ finetuning/finetunes.py
@@ -1,47 +1,34 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+#hugely aided by https://github.com/nrimsky/LM-exp/blob/main/intermediate_decoding/intermediate_decoding.ipynb
 
-def temp(hello):
-    return None
+import torch
+from cti.transformers.transformers.src.transformers.models.auto import AutoTokenizer, AutoModelForCausalLM
 
-model_name_or_path = "TheBloke/Llama-2-7b-Chat-GPTQ"
-# To use a different branch, change revision
-# For example: revision="gptq-4bit-64g-actorder_True"
-model = AutoModelForCausalLM.from_pretrained(model_name_or_path,
-                                             device_map="auto",
-                                             trust_remote_code=False,
-                                             revision="main")
+# class RETROWrapper(torch.nn.Module):
 
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+class BioLlama:
+    def __init__(self):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.tokenizer = AutoTokenizer.from_pretrained("TheBloke/Llama-2-13b-Chat-GPTQ")
+        self.model = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-13b-Chat-GPTQ",
+                                                          device_map="auto")
 
-prompt = "Tell me about AI"
-prompt_template=f'''[INST] <<SYS>>
-You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
-<</SYS>>
-{prompt}[/INST]
+        #here i change this part, so that im adding normal layers as normal
+        #and retro layers differently
+        # for i, layer in enumerate(self.model.model.layers):
+        #     self.model.model.layers[i] = BlockOutputWrapper(layer, self.model.lm_head, self.model.model.norm)
 
-'''
+        # RETRO_layer_ids = [15]
+        # for i, layer in enumerate(self.model.model.layers):
+        #     #switch decoder layer to be a RETRO layer
+        #     if i in RETRO_layer_ids:
+        #         self.model.model.layers[i] = RETROWrapper(layer, self.model.lm_head, self.model.model.norm)
 
-print("\n\n*** Generate:")
-
-input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
-output = model.generate(inputs=input_ids, temperature=0.7, do_sample=True, top_p=0.95, top_k=40, max_new_tokens=512)
-print(tokenizer.decode(output[0]))
-
-# Inference can also be done using transformers' pipeline
-
-# print("*** Pipeline:")
-# pipe = pipeline(
-#     "text-generation",
-#     model=model,
-#     tokenizer=tokenizer,
-#     max_new_tokens=64,
-#     do_sample=True,
-#     temperature=0.1,
-#     top_p=0.95,
-#     top_k=40,
-#     repetition_penalty=1.1
-# )
-
-# output = pipe(prompt_template)
-
-# print(output[0]['generated_text'])
+    def generate_text(self, prompt, max_length=30):
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        generate_ids = self.model.generate(inputs.input_ids.to(self.device), max_length=max_length)
+        return self.tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+    
+BioLlama = BioLlama()
+prompt = "Tell me a good night story"
+text = BioLlama.generate_text(prompt)
+print(text)
